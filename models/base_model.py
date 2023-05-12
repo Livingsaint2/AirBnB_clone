f4b711d24066ef76ebbd0a215453c9345ae87e97
Artista001 <samwuels003@gmail.com>
2023-05-12 22:25:48 +0000

    Task 3

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+   """This is the base class that all other classses inherit from"""
+
+   def __init__(self):
+       """Initializes instance attributes"""
+
+       self.id = str(uuid.uuid4())
+       self.created_at = datetime.now()
+       self.updated_at = datetime.now()
+
+   def __str__(self):
+       """returns human-readable string representation of an instance"""
+
+       class_name = self.__class__.__name__
+       return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+   def save(self):
+       """updates the public instance attribute updated_at
+          with the current datetime
+       """
+
+       self.updated_at = datetime.now()
+
+   def to_dict(self):
+       """Returns a dictionary containing all keys/values of __dict__
+          of the instance
+       """
+
+       ob_dict = self.__dict__.copy()
+       ob_dict['__class__'] = self.__class__.__name__
+       ob_dict['created_at'] = self.created_at.isoformat()
+       ob_dict['updated_at'] = self.updated_at.isoformat()
+
+       return ob_dict
