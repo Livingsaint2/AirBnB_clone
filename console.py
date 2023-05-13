77138dd626f3200961353fb1c17b0d758604f049
Artista001 <samwuels003@gmail.com>
2023-05-13 02:10:01 +0000

    Console.py, Tests

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+      """This is the class of the command interptreter"""
+
+      prompt = "(hbnb) "
+
+      def do_EOF(self, line):
+          """This is the end-of-file marker"""
+
+          return True
+
+      def do_quit(self, line):
+          """Quit command to exit the program"""
+
+          return True
+
+      def emptyline(self):
+          """This shouldn't do anything"""
+
+          pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
